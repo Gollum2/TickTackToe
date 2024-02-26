23336f43204dd8d55d6d1f891ef4c8e8fc221029
Jqnnik67 <Janniksn92@gmail.com>
2024-02-27 00:46:20 +0100

    Spielfeld initialization getUserInput

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,5 +1,16 @@
-public class Spielfeld {
-    Spielfeld(){
+import java.util.ArrayList;
 
+public class Spielfeld {
+    private ArrayList<ArrayList<Character>> spielfeld = new ArrayList<ArrayList<Character>>();
+    Spielfeld(){
+        for(ArrayList<Character> l : spielfeld){
+            l = new ArrayList<>();
+            l.add(' ');
+        }
+    }
+
+    public int getUserInput(){
+        System.out.println("Wähle dein Feld!");
+        return 0;
     }
 }
